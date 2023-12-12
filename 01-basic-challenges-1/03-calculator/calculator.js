0fd7f5e45c6f0e1147cfcbd8433b84c7b62aad61
AbdighaniMD <abdighanimd@outlook.com>
2023-12-12 17:39:10 +0000

    03-calculator

--- 01-basic-challenges-1/03-calculator/calculator.js
+++ 01-basic-challenges-1/03-calculator/calculator.js
@@ -1,3 +1,19 @@
-function calculator() {}
+function calculator(a, b, operator) {
+    let result
+    switch(operator){
+        case '+':
+            return result = a + b
+        case '-':
+            return result = a - b
+        case '*':
+            return result = a * b
+        case '/':
+            return result = a / b
+        default:
+            console.log("Invalid operator")
+            //throw new Error('Invalid operator');
+    }
+    
+}
 
 module.exports = calculator;
